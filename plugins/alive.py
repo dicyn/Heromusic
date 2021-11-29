46175f12ae81934e8b1af839828d3e0c9c654c72
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-11-29 10:57:31 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -31,7 +31,7 @@ async def _human_time_duration(seconds):
     
    
 
-@Client.on_message(command("candy") & filters.private & ~filters.edited)
+@Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/bf0881bcaeff4236a7257.jpg",
